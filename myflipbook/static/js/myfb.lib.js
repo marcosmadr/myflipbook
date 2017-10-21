05184682367ca098c398240a442f702186e82b57
marcosmadr <toguto@gmail.com>
2017-10-21 03:11:40 -0200

    changing myfb lib to use prototype

--- myflipbook/static/js/myfb.lib.js
+++ myflipbook/static/js/myfb.lib.js
@@ -1,16 +1,16 @@
 
 var myFlipBook = function(file, func) {
 
-    var allowed_ext = [ 'mp4', 'avi', 'mov', 'wmv', 'mkv' ];
-    var frames = [];
-    var frames_cover = undefined;
-    var position = 0;
-    var frame = { 
+    this.allowed_ext = [ 'mp4', 'avi', 'mov', 'wmv', 'mkv' ];
+    this.frames = [];
+    this.frame_cover = undefined;
+    this.position = 0;
+    this.frame = { 
         width: 340, 
         height: 233, 
         max: 49, //+1 frame at 0sec aka cover
     };
-    var callback_func = func;
+    this.callback_func = func;
 
     if (! file) {
         throw { name: 'FileInput', message: 'Missing input file' };
@@ -18,22 +18,68 @@ var myFlipBook = function(file, func) {
 
     var ext = file.name.split('.').pop().toLowerCase();
 
-    if($.inArray(ext, allowed_ext) == -1) {
+    if($.inArray(ext, this.allowed_ext) == -1) {
         throw { name: 'InvalidExtension', message: 'Invalid video type' };
     }
 
-    var fileURL = URL.createObjectURL(file);
-    var video = document.createElement("video");
-    video.src = fileURL;
+    this.fileURL = URL.createObjectURL(file);
+    this.video = document.createElement("video");
+    this.video.src = this.fileURL;
 
-    var createCover = function(text, font_color, font_size, callback_cover, deco) {
+    var that = this;
+
+    var createImage = function() {
+        var canvas = document.createElement("canvas");
+        canvas.width = that.frame.width;
+        canvas.height = that.frame.height;
+
+        var context = canvas.getContext('2d');
+        context.drawImage(that.video, 0, 0, canvas.width, canvas.height);
+
+        return canvas.toDataURL();
+    };
+   
+    this.video.addEventListener('seeked', function() {
+        that.frames.push(createImage()); 
+
+        that.position += that.increment;
+
+        if (that.position.toFixedDown(2) <= this.duration) {
+            this.currentTime = that.position;
+        } else {
+            that.callback_func(that.frames);
+        }
+
+    }, false);
+
+    this.video.addEventListener('loadeddata', function() {
+        if (isNaN(this.duration)) {
+            throw {
+                name: 'FormatError',
+                message: 'Cannot determine video length'
+            };
+        }
+        that.increment = this.duration / that.frame.max;
+        that.position = 0;
+        that.frames = [];
+
+        this.currentTime = that.position;
+    }, false);
+
+};
+
+myFlipBook.prototype.progress = function() {
+	    return Math.round((this.frames.length*100) / this.frame.max); 
+};
+
+myFlipBook.prototype.createCover = function(text, font_color, font_size, callback_cover, deco) {
         var canvas = document.createElement("canvas");
         var context = canvas.getContext('2d');
-        canvas.width = frame.width;
-        canvas.height = frame.height;
-
+        canvas.width = this.frame.width;
+        canvas.height = this.frame.height;
+        
         var coverImage = new Image();
-       
+        var that = this; 
         coverImage.onload = function() {
              context.drawImage(coverImage, 0, 0);
              if (deco) {
@@ -44,75 +90,28 @@ var myFlipBook = function(file, func) {
                  context.font = font_size+"pt Calibri";
                  context.fillText(text, 30, 50);
              }
+             that.frame_cover = canvas.toDataURL();
+
              if (callback_cover) {
-                callback_cover(canvas.toDataURL());
+                callback_cover(that.frame_cover);
              }
-             frames_cover = canvas.toDataURL();
         };
-        coverImage.src = frames[0];
-    };
-
-    var createImage = function() {
-        var canvas = document.createElement("canvas");
-        canvas.width = frame.width;
-        canvas.height = frame.height;
-
-        var context = canvas.getContext('2d');
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-        return canvas.toDataURL();
-    };
-   
-    video.addEventListener('seeked', function() {
-        frames.push(createImage()); 
-
-        position += increment;
-
-        if (position.toFixedDown(2) <= this.duration) {
-            this.currentTime = position;
-        } else {
-            callback_func(frames);
-        }
-
-    }, false);
-
-    video.addEventListener('loadeddata', function() {
-        if (isNaN(video.duration)) {
-            throw {
-                name: 'FormatError',
-                message: 'Cannot determine video length'
-            };
-        }
-        increment = video.duration / frame.max;
-        position = 0;
-        frames = [];
-
-        this.currentTime = position;
-    }, false);
-
-	Number.prototype.toFixedDown = function(n) {
-    	var x = String(this).split(".");
-	    if (! x[1] ) {
-	        return this;
-	    }
-	    return Number(x[0]+"."+String(x[1]).substring(0,n));
-	};
-
-	return  { 
-		progress: function() { 
-			return Math.round((frames.length*100) / frame.max); 
-		    },
-        frame: function() {
-            return frame;
-        },
-        frames: function() {
-            var new_frames = frames.slice();
-            new_frames[0] = frames_cover;
-            return new_frames;
-        },
-        createCover: function(text, font_color, font_size, callback_cover_func, deco ) {
-            return createCover(text, font_color, font_size, callback_cover_func, deco);
-        },
-    };
-
+        coverImage.src = this.frames[0];
 };
+
+myFlipBook.prototype.getFrames = function() {
+	    var newframes = this.frames.slice();
+        if (this.frame_cover) {
+            newframes[0] = this.frame_cover;
+        }
+        return newframes;
+};
+
+Number.prototype.toFixedDown = function(n) {
+    var x = String(this).split(".");
+    if (! x[1] ) {
+        return this;
+    }
+    return Number(x[0]+"."+String(x[1]).substring(0,n));
+};
+
