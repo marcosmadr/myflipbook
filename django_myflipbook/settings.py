e82198f8ac2d47bc44ed0a6f1346fa59c2211cc0
maugusto <toguto@gmail.com>
2018-06-07 21:52:17 -0300

    fix allowed hosts

--- django_myflipbook/settings.py
+++ django_myflipbook/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cqk7rwp50=bda5j#09*!g7%@7-e^o8h&_h9sb+qsf5qj3)tz&@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.9', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
